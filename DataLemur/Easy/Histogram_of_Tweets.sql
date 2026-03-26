87f376ab95c4559f4461c05eba74fb4662ce4027
Ahmed Ali <ahmaad.alii.213@gmail.com>
2026-03-26 09:55:00 +0300

    26/3/2026

---
+++ DataLemur/Easy/Histogram_of_Tweets.sql
@@ -0,0 +1,12 @@
+-- Using: Posgress
+WITH cte AS(
+  SELECT user_id , count(user_id) AS no_of_tweets
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = 2022
+  GROUP BY user_id
+)
+
+SELECT count(user_id) as tweet_bucket, no_of_tweets as users_num
+FROM cte
+GROUP BY no_of_tweets
+ORDER BY count(user_id)
